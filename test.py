9c6a81e00959924fbd5115e138339f2e518d329e
CooooldWind_ <3091868003@qq.com>
2023-12-02 17:30:11 +0800

    重新提交

---
+++ test.py
@@ -0,0 +1,19 @@
+'''
+version: 1.0.0.231112a
+docstring: 测试文件
+author: CooooldWind_
+'''
+import encode_sec_key
+import pprint
+
+pprint.pprint(
+    encode_sec_key.NeteaseParams(
+        url = "https://music.163.com/weapi/song/lyric?csrf_token=",
+        encode_data = {
+            'csrf_token': "",
+            'id': "1398764652",
+            'lv': '-1',
+            'tv': '-1'
+            }
+        ).get_resource()
+    )
