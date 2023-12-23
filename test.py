ae446c3f38dc3f1729e14d507e160429c34d8441
CooooldWind_ <3091868003@qq.com>
2023-12-23 20:38:44 +0800

    test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import ListDownloader
 import pprint
 
-test_p = ListDownloader.Playlist("2998665333")
+test_p = ListDownloader.Playlist("2391850012")
 test_p.get_resource()
 for i in test_p.tracks:
     i.get_resource()
     pprint.pprint(i.info)
+'''
+2998665333
+'''
