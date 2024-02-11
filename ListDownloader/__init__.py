f508ccd9d20dd34e446a2cc16c82b3e72f6609c7
CoooldWind <3091868003@qq.com>
2024-02-12 00:40:23 +0800

    多写了点

--- ListDownloader/__init__.py
+++ ListDownloader/__init__.py
@@ -4,8 +4,14 @@ Version: 1.0.0.231222a
 Author: CooooldWind_
 '''
 from . import encode_sec_key
-from .global_args import PLAYLIST_URL, SONG_INFO_URL
+from .global_args import PLAYLIST_API, SONG_INFO_API, SONG_FILE_API_2, LEVEL, LYRIC_API
+from PIL import Image
+from mutagen.flac import FLAC
+from mutagen.id3 import ID3,APIC
 import threading
+import requests
+import random
+import time
 
 class Playlist:
     '''Playlist类'''
@@ -20,7 +26,7 @@ class Playlist:
         '''获取数据'''
         result = encode_sec_key.NeteaseParams(
             encode_data = self.encode_data,
-            url = PLAYLIST_URL).get_resource()
+            url = PLAYLIST_API).get_resource()
         self.creater = result['playlist']['userId']
         self.track_id = result['playlist']['trackIds']
         self.tracks = [self.Song({'id': i['id']}, str(self.creater)) for i in self.track_id]
@@ -38,11 +44,76 @@ class Playlist:
             self.info = {}
         def get_resource(self):
             '''获取更加详细的数据'''
-            with threading.Semaphore(16):
-                result = encode_sec_key.NeteaseParams(
+            with threading.Semaphore(32):
+                pure_response = encode_sec_key.NeteaseParams(
                     encode_data = self.encode_data,
-                    url = SONG_INFO_URL).get_resource()['songs'][0]
-                self.info.update({'album': result['al']['name']})
-                self.info.update({'id': result['al']['id']})
-                self.info.update({'artists': i['name'] for i in result['ar']})
-                self.info.update({'name': result['name']})
+                    url = SONG_INFO_API)
+                self.response = pure_response.get_resource()['songs'][0]
+                self.info.update({'album': self.response['al']['name']})
+                self.info.update({'id': str(self.response['id'])})
+                self.info.update({'name': self.response['name']})
+                self.info.update({'cover_url': self.response['al']['picUrl']})
+                tmp = []
+                for i in self.response['ar']:
+                    tmp.append(i['name'])
+                self.info.update({'artist': tmp})
+        def song_download(self, level, dir, filename):
+            '下载音乐'
+            song_request_url = SONG_FILE_API_2 + "id=" + self.info['id'] + "&level=" + LEVEL[level + 1]
+            response = requests.get(url = song_request_url).json()['data'][0]
+            self.info.update({'song_url': response['url']})
+            music_file = open(dir + filename + '.' + response['type'], 'wb+')
+            rate = int(0)
+            source = requests.get(self.info['song_url'], stream = True, allow_redirects = True)
+            for data in source.iter_content(chunk_size = 1024):
+                music_file.write(data)
+                rate += len(data)
+                '别一直干活，人总是要休息的，服务器也一样，别累坏人家了'
+                if random.randint(0,1000) % 200 == 0:
+                    time.sleep(0.01)     
+            music_file.close()
+        def lyric_download(self, dir, filename):
+            '''下载歌词'''
+            lyric_file = open(dir + filename + '.lrc','w+',encoding = 'utf-8')
+            encode_data = {'csrf_token':"",
+                        'id':self.info['id'],
+                        'lv':'-1',
+                        'tv':'-1'}
+            response = encode_sec_key.NeteaseParams(
+                encode_data = encode_data,
+                url = LYRIC_API
+            ).get_resource()
+            lyric_file.write(response['lrc']['lyric'].replace("\n",'\n'))
+            lyric_file.close()
+        def cover_download(self, dir, filename, size = -1):
+            '''封面下载'''
+            cover_file = open(dir + filename + '.jpg', 'wb+')
+            source = requests.get(url = self.info['cover_url'], stream = True, allow_redirects = True)
+            rate = int(0)
+            for data in source.iter_content(chunk_size = 1024):
+                cover_file.write(data)
+                rate += len(data)
+                '别一直干活，人总是要休息的，服务器也一样，别累坏人家了'
+                if random.randint(0,1000) % 200 == 0:
+                    time.sleep(0.01)     
+            cover_file.close()
+            size = int(size)
+            if size == -1: return
+            cover_file = Image.open(dir + filename + '.jpg')
+            cover_file = cover_file.convert("RGB")
+            cover_file_type = cover_file.format
+            cover_file_out = cover_file.resize((size, size))
+            cover_file_out.save(dir + filename + '.jpg', cover_file_type)
+        def attribute_write(self, dir, filename, type, ):
+            if type == "flac":
+                music_file = FLAC(dir + filename + ".flac")
+                music_file['title'] = self.info['name']
+                music_file['album'] = self.info['album']
+                tmp = ""
+                for i in self.info['artist']:
+                    tmp = tmp + i
+                    if i != self.info['artist'][-1]: tmp = tmp + "; "
+                music_file['artist'] = tmp
+                music_file.save()
+                print(music_file.values())
+            
--- test.py
+++ test.py
@@ -3,6 +3,15 @@ import pprint
 
 test_p = ListDownloader.Playlist("2391850012")
 test_p.get_resource()
+'''
 for i in test_p.tracks:
     i.get_resource()
-    pprint.pprint(i.info)
+    pprint.pprint(i.response)
+'''
+
+test_p.tracks[0].get_resource()
+pprint.pprint(test_p.tracks[0].response)
+pprint.pprint(test_p.tracks[0].info)
+'''test_p.tracks[0].song_download(5, "C:\\Users\\Administrator\\Downloads\\", "test")'''
+'''test_p.tracks[0].cover_download("C:\\Users\\Administrator\\Downloads\\", "test2", size = 128)'''
+test_p.tracks[0].attribute_write("C:\\Users\\Administrator\\Downloads\\", "test", "flac")
