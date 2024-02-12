3dad1d1e4abf2a138c999f8f931f03fb5b75fd4f
CoooldWind <3091868003@qq.com>
2024-02-12 11:17:37 +0800

    更新部分文件

--- install_dependtions.bat
+++ install_dependtions.bat
@@ -1,7 +1,6 @@
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pip config set global.trusted-host mirrors.aliyun.com
 pip install pycryptodome
-pip install eyed3
 pip install pillow
 pip install mutagen
 pip install requests
