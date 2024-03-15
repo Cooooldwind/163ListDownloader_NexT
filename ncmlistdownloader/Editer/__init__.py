e663cde25ea3598bea8131fd33d43c7040a04bf2
CoooldWind <3091868003@qq.com>
2024-03-15 22:48:32 +0800

    24.3.15

--- ncmlistdownloader/Editer/__init__.py
+++ ncmlistdownloader/Editer/__init__.py
@@ -1,19 +1,30 @@
+'''
+ncmlistdownloader/Editer/__init__.py
+Core.Ver.1.0.0.240315a1
+Author: CooooldWind_
+Updated_Content:
+1. cover_write(filename, cover_filename)
+2. lyric_write(filename, lyric_filename)
+'''
 from PIL import Image
 from mutagen.flac import FLAC, Picture
 from mutagen.mp3 import EasyMP3
 from mutagen.id3 import ID3, APIC, USLT, Encoding
 from Common import get_type, artist_turn_str
 
+expection_word_front = "Opening files with the suffix \"."
+lyric_expection_word_front = "Opening cover files with the suffix \"."
+cover_expection_word_front = "Opening cover files with the suffix \"."
+expection_word_end = "\" is not supported."
+
 def attribute_write(filename = str(), info = dict()):
     '''
     属性写入
     ----------
     参数：
-    1. filename: 文件名，字符串
+    1. filename: 文件名，字符串，仅mp3/flac格式
     2. info: 信息，里面需要"album"，"artist"，"title"
     '''
-    expection_word_front = "Opening files with the suffix \"."
-    expection_word_end = "\" is not supported."
     type = get_type(filename)
     if type == "mp3":
         file = EasyMP3(filename)
@@ -29,4 +40,70 @@ def attribute_write(filename = str(), info = dict()):
     file.save()
     return 0
 
-    
+def cover_write(filename = str(), cover_filename = str()):
+    '''
+    专辑封面写入
+    ----------
+    参数：
+    1. filename: 文件名，字符串，仅mp3/flac格式
+    2. cover_filename: 封面的文件名，字符串，仅jpg格式
+    '''
+    type = get_type(filename)
+    if type == "mp3":
+        file = ID3(filename)
+    elif type == "flac":
+        file = FLAC(filename)
+    else: 
+        raise Exception(expection_word_front + type + expection_word_end)
+    cover_type = get_type(cover_filename)
+    if cover_type != "jpg" and cover_type != "jpeg":
+        raise Exception(cover_expection_word_front + cover_type + expection_word_end)
+    with open(cover_filename, 'rb+') as cover_file:
+        if type == "mp3":
+            file.add(APIC(
+                encoding = 3,
+                mime = "image/jpeg",
+                type = 3,
+                desc = u"Cover",
+                data = cover_file.read()
+            ))
+        elif type == "flac":
+            file.clear_pictures()
+            cover = Picture()
+            cover.data = cover_file.read()
+            cover.mime = "image/jpeg"
+            file.add_picture(cover)
+    file.save()
+    return 0
+
+def lyric_write(filename = str(), lyric_filename = str()):
+    '''
+    专辑封面写入
+    ----------
+    参数：
+    1. filename: 文件名，字符串，仅mp3/flac格式
+    2. lyric_filename: 封面的文件名，字符串，仅lrc格式
+    '''
+    type = get_type(filename)
+    if type == "mp3":
+        file = ID3(filename)
+    elif type == "flac":
+        file = FLAC(filename)
+    else: 
+        raise Exception(expection_word_front + type + expection_word_end)
+    lyric_type = get_type(lyric_filename)
+    if lyric_type != "lrc":
+        raise Exception(lyric_expection_word_front + lyric_type + expection_word_end)
+    with open(lyric_filename, 'rb+') as lyric_file:
+        lyric = lyric_file.read()
+        if type == "mp3":
+            file.setall("USLT",[USLT(
+                encoding = Encoding.UTF8,
+                format = 2,
+                type = 1,
+                text = lyric
+            )])
+        elif type == "flac":
+            file['lyrics'] = lyric
+    file.save()
+    return 0
