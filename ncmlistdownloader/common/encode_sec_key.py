3fe722b81bf1bee3d1a53f998ec24c1d00dfa8ae
CoooldWind <3091868003@qq.com>
2024-03-30 07:34:22 +0800

    rebuild folder 2

---
+++ ncmlistdownloader/common/encode_sec_key.py
@@ -0,0 +1,64 @@
+'''
+网易云WeAPI解码
+Core.Ver.1.0.0.240325a1
+Author: CooooldWind_, 半岛的孤城
+References: 
+1. 网易云解参数（获取网易云歌词，获取评论同理）[https://www.bilibili.com/read/cv12754897/]
+'''
+
+import random
+import json
+from base64 import b64encode
+import requests
+from Crypto.Cipher import AES
+from ncmlistdownloader.common.global_args import USER_AGENTS, FUNC_F, SEC_KEY
+
+class NeteaseParams:
+    '''
+    WeAPI解码类
+    ----------
+    参数：
+    1. `encode_data`: 传入的参数
+    2. `url`: API的URL
+    '''
+    def __init__(self, encode_data, url):
+        self.encode_data = encode_data
+        self.url = url
+        self.func_e = "010001"
+        self.func_f = FUNC_F
+        self.func_g = "0CoJUm6Qyw8W8jud"
+        self.func_i = "vlgPRPyGhwA6F4Sq"
+        self.encode_sec_key = SEC_KEY
+    def to_hex(self, encode_data):
+        '''16进制解码'''
+        temp = 16 - len(encode_data) % 16
+        return encode_data + chr(temp) * temp
+    def encode_params(self, encode_data, encode_key):
+        '''解码的关键函数(1)'''
+        func_iv = "0102030405060708"
+        encode_data = self.to_hex(encode_data)
+        encode_aes = AES.new(key = encode_key.encode("utf-8"),
+                            IV = func_iv.encode("utf-8"),
+                            mode = AES.MODE_CBC)
+        base64_sec_key = encode_aes.encrypt(encode_data.encode("utf-8"))
+        return str(b64encode(base64_sec_key), "utf-8")
+    def get_params(self, encode_data):
+        '''解码的关键函数(2)'''
+        return self.encode_params(
+            self.encode_params(
+                encode_data, self.func_g
+            ), self.func_i
+        )
+    def get_resource(self):
+        '''获取资源'''
+        get_data = {
+            'params': self.get_params(json.dumps(self.encode_data)),
+            'encSecKey': self.encode_sec_key
+        }
+        get_headers = {
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        return requests.post(self.url,
+                             data = get_data,
+                             headers = get_headers,
+                             timeout = 10).json()
---
+++ ncmlistdownloader/downloader/__init__.py
@@ -0,0 +1,119 @@
+'''
+ncmlistdownloader/Downloader/__init__.py
+Core.Ver.1.0.0.240328a1
+Author: CooooldWind_
+'''
+
+import random
+import time
+import threading
+import requests
+from requests.adapters import HTTPAdapter
+from ncmlistdownloader.common import *
+from ncmlistdownloader.common.encode_sec_key import *
+
+def calc_divisional_range(total_size, chunk_sum):
+    '''
+    calc_divisional_range
+    '''
+    step = total_size // chunk_sum
+    arr = list(range(0, total_size, step))
+    result = []
+    for i in range(len(arr) - 1):
+        s_pos, e_pos = arr[i], arr[i + 1] - 1
+        result.append([s_pos, e_pos])
+    result[-1][-1] = total_size - 1
+    return result
+
+class OriginFile:
+    '''
+    OriginFile
+    '''
+    def __init__(self, url = ""):
+        self.headers = dict(requests.head(url = url).headers)
+        self.total_size = int(self.headers['Content-Length'])
+        self.chunks = calc_divisional_range(self.total_size, 10)
+        self.url = url
+
+    def start(self, stream = True, max_retries = 3, thread_sum = 4, filename = str()):
+        '''
+        start
+        '''
+        downloader_list = []
+        for i in self.chunks:
+            downloader_list.append(Downloader(url = self.url,
+                                              chunk = i,
+                                              stream = stream,
+                                              max_retries = max_retries,
+                                              thread_sum = thread_sum,
+                                              filename = filename))
+        for i in downloader_list:
+            i.start()
+
+class Downloader(threading.Thread):
+    '''
+    每个部分文件的下载
+    '''
+    def __init__(self,
+                 url = "",
+                 chunk = None,
+                 stream = True,
+                 max_retries = 3,
+                 thread_sum = 4,
+                 filename = str()):
+        if not chunk:
+            raise ValueError("\"chunk\" must be a list.")
+        threading.Thread.__init__(self)
+        self.start_pos = chunk[0]
+        self.end_pos = chunk[1]
+        self.session = requests.Session()
+        self.session.mount('http://', HTTPAdapter(max_retries = max_retries))
+        self.session.mount('https://', HTTPAdapter(max_retries = max_retries))
+        self.source = self.session.get(url = url,
+                                  stream = stream,
+                                  allow_redirects = True,
+                                  timeout = (5,10),
+                                  headers = {
+                                      "Range": f"bytes={self.start_pos}-{self.end_pos}"
+                                  })
+        self.headers = dict(self.source.headers)
+        self.total_size = int(self.headers['Content-Length'])
+        self.thread_sum = thread_sum
+        self.filename = filename
+
+    def run(self):
+        '''
+        run
+        s'''
+        with threading.Semaphore(self.thread_sum):
+            with open(self.filename, 'wb+') as file:
+                file.seek(self.start_pos)
+                rate = 0
+                for data in self.source.iter_content(chunk_size = 1024):
+                    file.write(data)
+                    rate += len(data)
+                    if random.randint(0,1000) % 200 == 0:
+                        time.sleep(0.01)
+
+'''
+def download(url = "", filename = "", stream = True, max_retries = 3):
+    session = requests.Session()
+    session.mount('http://', HTTPAdapter(max_retries = max_retries))
+    session.mount('https://', HTTPAdapter(max_retries = max_retries))
+    with open(filename, 'wb') as file:
+        rate = 0
+        source = session.get(url = url,
+                              stream = stream,
+                              allow_redirects = True,
+                              timeout = (5, 15))
+        if stream:
+            for data in source.iter_content(chunk_size = 1024):
+                file.write(data)
+                rate += len(data)
+                if random.randint(0,1000) % 200 == 0:
+                    time.sleep(0.01)
+        else: file.write(source.content)
+    
+def response_get(url = "", data = dict()):
+    return NeteaseParams(url = url, encode_data = data).get_resource()
+'''
