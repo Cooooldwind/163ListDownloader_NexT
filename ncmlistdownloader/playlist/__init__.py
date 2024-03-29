579d66f15975bdd7c556dcdfe7a827e33f8b9d6a
CoooldWind <3091868003@qq.com>
2024-03-30 07:51:58 +0800

    Core.Ver.1.0.0.240330a2

--- ncmlistdownloader/playlist/__init__.py
+++ ncmlistdownloader/playlist/__init__.py
@@ -1,7 +1,9 @@
 '''
 ncmlistdownloader/Playlist/__init__.py
-Core.Ver.1.0.0.240330a1
+Core.Ver.1.0.0.240330a2
 Author: CooooldWind_
+Update_Context:
+1. Playlist.get_info()
 '''
 from ncmlistdownloader.common import *
 from ncmlistdownloader.common.encode_sec_key import *
@@ -16,6 +18,7 @@ class Playlist():
         self.raw_info = {}
         self.track_count = int(0)
         self.creator = ""
+        self.track_id = []
     def get_info(self):
         self.raw_info = NeteaseParams(url = PLAYLIST_API,
                                       encode_data = {
@@ -25,4 +28,6 @@ class Playlist():
         self.creator_id = self.raw_info['userId']
         self.track_count = self.raw_info['trackCount']
         self.creator = self.raw_info['creator']['nickname']
+        for i in self.raw_info['playlist']['trackIds']:
+            self.track_id.append(str(i['id']))
         return self.raw_info
