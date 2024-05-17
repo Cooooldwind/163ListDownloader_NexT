7accdbcb3f429ff305e081abc457b2eac6d1afc6
CoooldWind <3091868003@qq.com>
2024-05-17 22:26:06 +0800

    2.0.0.240517a1_tmp01

---
+++ ncmlistdownloader/encode.py
@@ -0,0 +1,60 @@
+import random
+import json
+from base64 import b64encode
+import requests
+from Crypto.Cipher import AES
+from ncmlistdownloader.global_args import USER_AGENTS, FUNC_F, SEC_KEY
+
+
+class NeteaseParams:
+    """
+    WeAPI解码类
+    ----------
+    参数：
+    1. `encode_data`: 传入的参数
+    2. `url`: API的URL
+    """
+
+    def __init__(self, encode_data, url):
+        self.encode_data = encode_data
+        self.url = url
+        self.func_e = "010001"
+        self.func_f = FUNC_F
+        self.func_g = "0CoJUm6Qyw8W8jud"
+        self.func_i = "vlgPRPyGhwA6F4Sq"
+        self.encode_sec_key = SEC_KEY
+
+    def _to_hex(self, encode_data):
+        """16进制解码"""
+        temp = 16 - len(encode_data) % 16
+        return encode_data + chr(temp) * temp
+
+    def _encode_params(self, encode_data, encode_key):
+        """解码的关键函数(1)"""
+        func_iv = "0102030405060708"
+        encode_data = self.__to_hex(encode_data)
+        encode_aes = AES.new(
+            key=encode_key.encode("utf-8"),
+            IV=func_iv.encode("utf-8"),
+            mode=AES.MODE_CBC,
+        )
+        base64_sec_key = encode_aes.encrypt(encode_data.encode("utf-8"))
+        return str(b64encode(base64_sec_key), "utf-8")
+
+    def _get_params(self, encode_data):
+        """解码的关键函数(2)"""
+        return self.__encode_params(
+            self.__encode_params(encode_data, self.func_g), self.func_i
+        )
+
+    def get_resource(self, cookies=None):
+        """获取资源"""
+        get_data = {
+            "params": self.__get_params(json.dumps(self.encode_data)),
+            "encSecKey": self.encode_sec_key,
+        }
+        get_headers = {"User-Agent": random.choice(USER_AGENTS)}
+        response = requests.post(
+            self.url, data=get_data, headers=get_headers, cookies=cookies, timeout=10
+        )
+        return response.json()
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from ncmlistdownloader.global_args import CORE_VERSION
+from ncmlistdownloader.encode import *
 
 print(CORE_VERSION)
