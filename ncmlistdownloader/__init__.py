3c7957267e8a075006cb15e59d2a83bec6a0b8cc
CooooldWind_ <3091868003@qq.com>
2024-07-05 22:56:07 +0800

    Ver.1.2.0.240705

--- ncmlistdownloader/__init__.py
+++ ncmlistdownloader/__init__.py
@@ -1,6 +1,6 @@
 """
 ncmlistdownloader/__init__.py
-Ver.1.1.4.240703
+Ver.1.2.0.240705
 Author: CooooldWind_
 """
 
@@ -9,6 +9,10 @@ import time
 from ncmlistdownloader.playlist import *
 from ncmlistdownloader.common import *
 from ncmlistdownloader.common.global_args import *
+from ncmlistdownloader.editer import cover_compress
+
+def t_or_f(s = str()) -> bool:
+    return (s.capitalize == "True" or s.capitalize == "T" or s == "")
 
 
 def main():
@@ -44,6 +48,17 @@ def main():
     while p.mp_succeed == False:
         time.sleep(1)
     l_finally = ""
+    print("Make some settings!")
+    # cover_download, lyric_get, attribute_write, lyric_write, cover_compress, gb18030/utf-8
+    bb,cc,dd,ee,ff,gg = True,True,True,True,-1,True
+    bb = t_or_f(str(input("1: cover_download & write(T/F): ")))
+    cc = t_or_f(str(input("2: lyric_get(T/F): ")))
+    dd = t_or_f(str(input("3: attribute_write(T/F): ")))
+    ee = t_or_f(str(input("4: lyric_write(T/F): ")))
+    ff = int(input("5: cover_compress(int): "))
+    if ff <= 0: ff = -1
+    gg = t_or_f(str(input("6: gb18030/utf-8(T/F): ")))
+    print(bb,cc,dd,ee,ff,gg)
     if c["MUSIC_U"] != "":
         l_str = ["standard", "higher", "exhigh", "lossless"]
         l = int(input("You have Cookies. Input the level(1~4): "))
@@ -61,11 +76,16 @@ def main():
         if music_filename == -1:
             print(i.title + " cannot download.")
             continue
-        cover_filename = i.cover_download()
-        lyric_filename = i.lyric_get()
-        i.attribute_write(music_filename)
-        i.cover_write(music_filename, cover_filename)
-        i.lyric_write(music_filename, lyric_filename)
+        if bb: cover_filename = i.cover_download()
+        if cc: 
+            if not gg: lyric_filename = i.lyric_get("utf-8")
+            else: lyric_filename = i.lyric_get()
+        if dd: i.attribute_write(music_filename)
+        if ff != -1: cover_compress(ff, cover_filename)
+        if bb: i.cover_write(music_filename, cover_filename)
+        if ee: i.lyric_write(music_filename, lyric_filename)
         n += 1
         print(f"{n}/{p.track_count}: {i.title} Succeed.")
     print("Succeed. Files at:", d)
+
+if __name__ == "__main__": main()
--- ncmlistdownloader/common/version.py
+++ ncmlistdownloader/common/version.py
@@ -1,9 +1,9 @@
 """
 list_downloader/global_args.py
-Core.Ver.1.1.4.240703
+Core.Ver.1.2.0.240705
 Author: CooooldWind_
 """
 
-CMD_VERSION = "Ver.1.1.4.240703"
-CORE_VERSION_SETUP = "1.1.4.240703"
+CMD_VERSION = "Ver.1.2.0.240705"
+CORE_VERSION_SETUP = "1.2.0.240705"
 CORE_VERSION = "Core.Ver." + CORE_VERSION_SETUP
