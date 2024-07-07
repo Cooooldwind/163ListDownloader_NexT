49455d9a41cc14ed50b7e5800604126c8c9f6f2f
CoooldWind <3091868003@qq.com>
2024-07-07 09:46:49 +0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "Programming Language :: Python :: 3.12",
     ],
     name="ncmlistdownloader",
-    version="1.1.4.240703",
+    version="1.2.0.240705",
     description="获取网易云音乐歌单数据，下载音乐，主动添加元信息。",
     author="CooooldWind_",
     url="https://gitee.com/Cooooldwind/163ListDownloader_NexT",
